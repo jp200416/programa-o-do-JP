bbfffc5e6046502bf100f2cfbc4ff5b667f3f84f
jp200416 <jjoaopaulo.m3@gmail.com>
2025-04-30 17:40:02 -0300

    mudancas

--- 2025-04-30/notas ifrn.py
+++ 2025-04-30/notas ifrn.py
@@ -3,3 +3,11 @@
 # 1) soicitar ao usuário que imforme duas notas (As notas são inteiras)
 # 2) calcular  a média do ifrn
 # 3) exibir a média sem casa decimal
+
+etapa1 = int(input('nota1:.'))
+etapa2 = int(input('nota1:.'))
+
+
+media = (etapa1 * 2 + etapa2 * 3) / 5
+print(f"media:{media}")
+
