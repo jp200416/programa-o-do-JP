baf497cdee8f07bacd494e3d075439297820b771
jp200416 <jjoaopaulo.m3@gmail.com>
2025-05-07 17:37:58 -0300

    salva

--- 2025-04-07/questao02.py
+++ 2025-04-07/questao02.py
@@ -6,13 +6,15 @@ distancia = int(input('20km'))
 if distancia <=0:
     sys.exit('informe distancia positiva')
 v_mi = int(input('12km/h'))
-if v_ini <=0:
+if v_mi <=0:
     sys.exit('informe velocidade positiva')
-aceleração = int(imput('75m/s'))
+aceleração = int(input('75m/s'))
 if aceleração <=0:
     sys.exit('informe aceleração positiva')
 distancia *= 1000
 v_mi /=3.6
 delta = v_mi**2-4*aceleração*distancia
 if delta <0:
-    sys.exit
+    sys.exit('não é possivel calcular o tempo')
+
+tempo = (-v_mi+delta**0.5)/(2*aceleração)
