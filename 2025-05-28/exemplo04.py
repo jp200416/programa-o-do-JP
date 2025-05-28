f4b56ca3a3d1b3e938586cfa1afc3d98121dd5db
jp200416 <jjoaopaulo.m3@gmail.com>
2025-05-28 17:22:40 -0300

    exemplos04

---
+++ 2025-05-28/exemplo04.py
@@ -0,0 +1,13 @@
+'''
+programa que solicite ao usuario numeros inteiros aleatoriamente
+até que seja informado o valor 0
+
+quando for digitado valor 0, o programa deverá informar:
+
+a) quantos numeros inteiros foram digitados;
+b) a soma dos numeros inteiros posistivos;
+c) a media dos numeros inteiros positivos;
+
+o valor 0 digitado nao deve ser considerado em nenhum dos itens acima.
+'''
+
