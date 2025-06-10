e6113a927c5133701dc15dd43745ad6b9f71e9ab
jp200416 <jjoaopaulo.m3@gmail.com>
2025-06-10 17:50:12 -0300

    salve

--- 2025-06-10/exemplo03.py
+++ 2025-06-10/exemplo03.py
@@ -1,1 +1,11 @@
-strText = input('diigte um texto: ')
+strTexto = input('diigte um texto: ')
+
+strvogais = 'aeiouãõáéíóú'
+
+vogais = 0
+
+for strLetra in strTexto:
+    if strLetra in strvogais:
+        vogais += 1
+
+print(f'o texto possui {vogais} vogais')
